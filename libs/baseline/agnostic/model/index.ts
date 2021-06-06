ed7b08f3c8aee218e017ced0f375d4ebc5006b95
ypolosov <ypolosov@gmail.com>
2021-06-06 22:11:56 +0300

    add doc

--- libs/baseline/agnostic/model/index.ts
+++ libs/baseline/agnostic/model/index.ts
@@ -1,11 +1,28 @@
 export * from './model';
 
 
-// baseline/agnostic/model
-// baseline/agnostic/fake
+// baseline/agnostic                              - a functional package namespace
+// baseline/agnostic/model                        - 'model' is a functional package api entry point
+// baseline/agnostic/fake                         - 'fake' is an api fake implementation entry point
+// baseline/agnostic/initial                      - 'initial' is a functional package application setup namespace
 // baseline/agnostic/initial/src
 // baseline/agnostic/initial/testing
-// baseline/agnostic/impl/src
-// baseline/agnostic/impl/testing
+// baseline/agnostic/impl                         - 'impl' is an implementations namespace
+// baseline/agnostic/impl/src                     - 'src' is an implementation entry point
+// baseline/agnostic/impl/testing                 - 'testing' is a testing stuff entry point 
+// baseline/agnostic/impl/mock                    - 'mock' is a functional package mocking namespace
 // baseline/agnostic/impl/mock/src
 // baseline/agnostic/impl/mock/testing
+// baseline/agnostic/impl/<alternative>           - 'alternative' is one more alt implementantion namespace
+// baseline/agnostic/impl/<alternative>/src
+// baseline/agnostic/impl/<alternative>/testing
+
+// baseline/agnostic/<xxxxx>                      - 'xxxxx' is one more functional package
+// baseline/agnostic/<xxxxx>/model
+// baseline/agnostic/<xxxxx>/fake
+// baseline/agnostic/<xxxxx>/initial/src
+// baseline/agnostic/<xxxxx>/initial/testing
+// baseline/agnostic/<xxxxx>/impl/src
+// baseline/agnostic/<xxxxx>/impl/testing
+// baseline/agnostic/<xxxxx>/impl/mock/src
+// baseline/agnostic/<xxxxx>/impl/mock/testing
