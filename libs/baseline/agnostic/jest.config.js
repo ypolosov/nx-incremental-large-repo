1e5a0ba47671642131ab8b32988ad5d8160c679a
ypolosov <ypolosov@gmail.com>
2021-06-08 16:57:57 +0300

    migrate to angular 12

--- libs/baseline/agnostic/jest.config.js
+++ libs/baseline/agnostic/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/baseline/agnostic/impl',
-  snapshotSerializers: [
-  ],
+
   displayName: 'baseline/agnostic',
+  snapshotSerializers: [],
 };
